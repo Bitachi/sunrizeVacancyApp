d6bff41d48c39beeff5053c595b8e7d0cc1bfb1a
GoTakami <funkykong888@gmail.com>
2023-04-10 21:01:53 +0000

    コードの重複を削除

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "サンライズ空席履歴"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "ホーム | #{@base_title}"
   end
   
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "問合せ | #{@base_title}"
   end
 end
