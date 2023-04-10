d20b211d6b970def023b05b1de57264418a77410
GoTakami <funkykong888@gmail.com>
2023-04-10 21:05:57 +0000

    Completed base of pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "サンライズ空席履歴"
   end
   
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "ホーム | #{@base_title}"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
